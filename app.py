f04a3baa7bbb2dcf6e23bfc09ed632e73f5ffc60
Lyubimov Denis <lyubimov.denis@gmail.com>
2014-01-04 19:07:12 +0400

    Added imports

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask,  request,  jsonify 
+import json
 
 
 app = Flask(__name__)
@@ -8,6 +9,45 @@ def index():
 
     return "Hello,world"
 
+def to_db(value):
+
+    with open('database.txt', 'w') as db:
+        db.write(value)
+
+def get_from_db():
+
+    with open('database.txt') as db:
+        data = db.read()
+    return data
+
+@app.route('/api',  methods = ['GET', 'PUT'])
+def api():
+
+    if request.method == "GET":
+
+        return jsonify(
+            {'data': get_from_db()})
+
+    elif request.method == "PUT":
+
+        data = request.get_json(force=True)
+
+        to_db(data['key'])
+
+
+        return jsonify({'status':
+                        "Data updated"})
+
+        
+
+                        
+        
+
+    
+
+
+        
+    
 if __name__ == '__main__':
 
     app.run(debug=True)
